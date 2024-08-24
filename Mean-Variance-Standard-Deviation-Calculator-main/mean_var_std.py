2beeb0912b12f8540ef8c3ba7ba0d1a185daa22f
Manoj Kumar K S <98869086+manojkumarks8@users.noreply.github.com>
2024-08-24 18:37:08 +0530

    Add files via upload

---
+++ Mean-Variance-Standard-Deviation-Calculator-main/mean_var_std.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def calculate(input_list):
+    if len(input_list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    matrix = np.array(input_list).reshape(3, 3)
+    calculations = {
+        'mean': [np.mean(matrix, axis=0).tolist(), np.mean(matrix, axis=1).tolist(), np.mean(matrix).tolist()],
+        'variance': [np.var(matrix, axis=0).tolist(), np.var(matrix, axis=1).tolist(), np.var(matrix).tolist()],
+        'standard deviation': [np.std(matrix, axis=0).tolist(), np.std(matrix, axis=1).tolist(), np.std(matrix).tolist()],
+        'max': [np.max(matrix, axis=0).tolist(), np.max(matrix, axis=1).tolist(), np.max(matrix).tolist()],
+        'min': [np.min(matrix, axis=0).tolist(), np.min(matrix, axis=1).tolist(), np.min(matrix).tolist()],
+        'sum': [np.sum(matrix, axis=0).tolist(), np.sum(matrix, axis=1).tolist(), np.sum(matrix).tolist()]
+    }
+    
+    return calculations
